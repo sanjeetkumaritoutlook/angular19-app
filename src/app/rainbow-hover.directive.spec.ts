c75bd3256e5f8334e4b27c0fad397c1c98fd6328
unknown <sanjeetkumarit@outlook.com>
2025-01-18 00:44:11 +0530

    fixed one unit test case of jasmine-karma

--- src/app/rainbow-hover.directive.spec.ts
+++ src/app/rainbow-hover.directive.spec.ts
@@ -2,7 +2,9 @@ import { RainbowHoverDirective } from './rainbow-hover.directive';
 
 describe('RainbowHoverDirective', () => {
   it('should create an instance', () => {
-    const directive = new RainbowHoverDirective();
+    const elRefMock = jasmine.createSpyObj('ElementRef', ['nativeElement']);
+    const rendererMock = jasmine.createSpyObj('Renderer2', ['setStyle']);
+    const directive = new RainbowHoverDirective(elRefMock, rendererMock); //2 arguments required because of initilization in constructor
     expect(directive).toBeTruthy();
   });
 });
